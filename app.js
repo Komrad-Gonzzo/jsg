010d33571086762496ad345d2b071096769978fe
Komrad-Gonzzo <crst.eliseev@gmail.com>
2022-10-20 16:03:12 +0300

    add score and finish

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
-let time = 20
+let time = 0
+let score = 0
 
 startBtn.addEventListener('click', (event)=> {
     event.preventDefault()
@@ -18,8 +19,13 @@ timeList.addEventListener('click', event => {
     }
 })
 
-//DEBUG
-startGame()
+board.addEventListener('click', event => {
+    if(event.target.classList.contains('circle')){
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
 
 function startGame() {
     setInterval(decreaseTime, 1000)
@@ -44,15 +50,26 @@ function setTime(value) {
 }
 
 function finishGame() {
-
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Cчёт: <span class="primary">${score}</span></h1>`
 }
 
 function createRandomCircle() {
     const circle = document.createElement ('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
 
     circle.classList.add('circle')
-    circle.style.width = '15px'
-    circle.style.height = '15px'
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
 
     board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max-min) + min)
 }
